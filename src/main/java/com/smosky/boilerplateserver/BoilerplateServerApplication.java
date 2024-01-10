c027794196fcf5120057c38c9a2ef3783f113ea9
tranphuocloc070699 <tranphuocloc070699@gmail.com>
2024-01-10 23:19:26 +0700

    home

---
+++ src/main/java/com/smosky/boilerplateserver/BoilerplateServerApplication.java
@@ -0,0 +1,13 @@
+package com.smosky.boilerplateserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smosky/boilerplateserver/BoilerplateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smosky.boilerplateserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
